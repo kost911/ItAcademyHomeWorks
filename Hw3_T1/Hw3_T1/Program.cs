c446b26a7bc67b5e2ca4e44e2caf6d33ff7b034b
Yatsynovich Kanstantsin <kostbestofthebest@gmail.com>
2019-10-01 14:00:35 +0300

    Hw3_T1

---
+++ Hw3_T1/Hw3_T1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw3_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number");
+            int fnum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter the second number");
+            int snum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(fnum+snum);
+            Console.ReadKey();
+        }
+    }
+}
