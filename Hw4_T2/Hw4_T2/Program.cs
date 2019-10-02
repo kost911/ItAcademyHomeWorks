f70450f9c023a03f0863517fb6e9a73d1de6185a
Yatsynovich Kanstantsin <kostbestofthebest@gmail.com>
2019-10-03 01:27:20 +0300

    Hw4_T2

---
+++ Hw4_T2/Hw4_T2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw4_T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("select the direction of movement W A S D");
+            string direction = Convert.ToString(Console.ReadLine());
+            switch (direction)
+            {
+                case "W":
+                    Console.WriteLine("upward movement selected");
+                    break;
+                case "A":
+                    Console.WriteLine("left movement selected");
+                    break;
+                case "S":
+                    Console.WriteLine("right movement selected");
+                    break;
+                case "D":
+                    Console.WriteLine("downward movement selected");
+                    break;
+                default:
+                    Console.WriteLine("error in direction selection");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
